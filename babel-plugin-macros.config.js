a188c0519bea4c69d4297f154bd31119dace9690
Fenotiana Andriamahenimanana <me.mans.andriam@gmail.com>
2021-07-23 19:14:53 +0300

    Create Button component
    Install twin.macro, styled-component
    Add twin.macro config
    Create Button component with tailwind and styled component

---
+++ babel-plugin-macros.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    twin: {
+      preset: 'styled-components',
+      styled: {
+        import: 'default',
+        from: 'styled-components',
+      },
+      css: {
+        import: 'css',
+        from: 'styled-components',
+      },
+    },
+  }
